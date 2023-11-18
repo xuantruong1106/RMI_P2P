8dd9a1b87f9a39281f3520639c0023dd9e0b3748
xuantruong1106 <luomtruong11@gmail.com>
2023-11-18 12:53:57 +0700

    update 2

--- src/main/java/com/mycompany/rmi_p2p/Client.java
+++ src/main/java/com/mycompany/rmi_p2p/Client.java
@@ -1,27 +1,31 @@
 package com.mycompany.rmi_p2p;
+
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.Scanner;
+
 public class Client {
+
     public static void main(String[] args) {
         try {
             Scanner in = new Scanner(System.in);
             System.out.print("Enter your name: ");
-            String name = in.nextLine().trim(); 
-            IServer chatServer = (IServer)Naming.lookup("rmi://localhost/ABC");
+            String name = in.nextLine().trim();
+            IServer chatServer = (IServer) Naming.lookup("rmi://localhost/ABC");
             ClientImp chatClient = new ClientImp();
             UnicastRemoteObject.exportObject(chatClient, 0);
             chatServer.registerClient(name, chatClient);
             String msg;
             msg = "[" + name + "] got connected";
-            chatServer.unicastMessage(msg);          
+            chatServer.unicastMessage(msg);
             while (true) {
                 msg = in.nextLine().trim();
-                if(msg.equals("exit"))
+                if (msg.equals("exit")) {
                     break;
+                }
                 msg = "[From " + name + "] " + msg;
                 chatClient.client.retrieveMessage(msg);
             }
